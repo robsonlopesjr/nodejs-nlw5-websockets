b9e9c3dad5e5babd7b671b118661e21fefe7d958
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-04-19 13:09:17 -0300

    chore: add and configure typescript

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  return response.json({message: 'Olá NLW 05'});
+});
+
+app.post('/users', (request, response) => {
+  return response.json({message: 'Usuário salvo com sucesso!'});
+});
+
 app.listen(3333, () => {
   console.log('Server is running...');
 });
