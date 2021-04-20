c5dee24722a5641419545bcd4a0a3a2f6df754fa
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-04-20 09:59:24 -0300

    chore: add repository settings

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,6 @@ import './database';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Olá NLW 05'});
-});
-
-app.post('/users', (request, response) => {
-  return response.json({message: 'Usuário salvo com sucesso!'});
-});
-
 app.listen(3333, () => {
   console.log('Server is running...');
 });
